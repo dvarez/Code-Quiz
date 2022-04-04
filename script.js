8a55d20a22611d4f37033314d0fa13b204790b38
Dietrich Varez <dietrich1899@gmail.com>
2022-04-03 20:27:20 -0700

    format

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("hello world")
+// when i click the start button a timer starts and a question is presented.
+// when a question is answered another question is presented.
+// when a question is answered incorrectly the time is subtracted from the clock.
+// WHEN all questions are answered or the timer reaches 0 then the game is over.
+// when the game is over, i can save my initials and my score.
+ 
+
