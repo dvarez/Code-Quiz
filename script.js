1fe086569ab5a1cdb5c5b2c19c266ea9207cefc1
Dietrich Varez <dietrich1899@gmail.com>
2022-04-03 22:08:41 -0700

    question

--- script.js
+++ script.js
@@ -7,27 +7,25 @@ console.log("hello world")
  
 
 var questions = [
-
+    {
     question: "what is basic building block language of coding?"
     answers: ["word", "excel", "typing", "html"], 
     correctAnswer: "html"
-
+    },
+    {
     question: "what does CSS mean?"
     answers: ["Cascading Style sheets", "confirmed style subject", "color style sheets", "corona style subject"], 
     correctAnswer: "Cascading Style sheets"
-
+    },   
+    {
     question: "what does html mean?"
     answers: ["hyper text markup language", "hot top medium language", "hot to manage lists", "hot top markup language"], 
     correctAnswer: "hyper text markup language"
-
+    },    
+    {
     question: "what is a dinosaur?"
     answers: ["dog", "cat", "horse", "t-Rex"], 
     correctAnswer: "t-Rex"
-
-
-
-
-
-
+    },    
 
 ]
