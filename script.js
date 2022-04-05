b5c3ece07c72f060dcabe40b02b11d7ab0535bdc
Dietrich Varez <dietrich1899@gmail.com>
2022-04-05 09:03:36 -0700

    stuff

--- script.js
+++ script.js
@@ -79,3 +79,25 @@ if (e.target.innerHTML==questions[currentQuest].correctAnswer) {
 
 });
 
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (isWin && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          winGame();
+        }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        loseGame();
+      }
+    }, 1000);
+  }
